161f3d60eac516cf16b7123ec7370dff64cb60a9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-10-04 12:17:41 -0500

    Loop de WordPress para listar el contenido de páginas

--- wp-content/themes/magazine-toronto/page.php
+++ wp-content/themes/magazine-toronto/page.php
@@ -1,1 +1,12 @@
-<h2>Page.php</h2>
+<?php if ( have_posts() ) : ?>
+
+	<?php while ( have_posts() ) : the_post(); ?>
+		<?php // Individual Post Styling ?>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+	<?php endwhile; ?>
+
+	<?php // Navigation ?>
+<?php else : ?>
+	<?php // No Posts Found ?>
+<?php endif; ?>
