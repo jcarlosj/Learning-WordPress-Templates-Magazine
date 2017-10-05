dbcdc5054f55955e8ef1a8c28ad8e583882533fa
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-10-05 16:43:03 -0500

    Agrega la funcionalidad de detectar cuando se tiene una imagen destacada o no (plantilla páginas 'page.php')

--- wp-content/themes/magazine-toronto/page.php
+++ wp-content/themes/magazine-toronto/page.php
@@ -2,11 +2,18 @@
 
 <?php if ( have_posts() ) : ?>
 	<?php while ( have_posts() ) : the_post(); ?>
+
 		<?php // Individual Post Styling ?>
-		<div id="imagen-destacada">
-			<?php the_post_thumbnail( 'imagen-destacada' ); ?>
-			<h2><?php the_title(); ?></h2>
-		</div>
+		<?php if( has_post_thumbnail() ) : ?>
+			<div id="imagen-destacada">
+				<?php the_post_thumbnail( 'imagen-destacada' ); ?>
+				<h2><?php the_title(); ?></h2>
+			</div>
+		<?php else: ?>
+			<h2 id="no-imagen-destacada">
+				<?php the_title(); ?>
+			</h2>
+		<?php endif; ?>
 
 		<div id="primary" class="primary">
 	        <?php the_content(); ?>
