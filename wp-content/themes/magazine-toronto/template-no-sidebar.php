23cbf88dc70df7ea1270c95f8ce380e2e5ae3537
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-10-07 12:48:08 -0500

    Crea plantilla personalizada para la Página de Contacto (template-no-sidebar.php)
    Adicional:
     - Instala 'plugin: Contact Form 7' (Creación de formularios)
     - Agrega los estilos para esta nueva plantilla

---
+++ wp-content/themes/magazine-toronto/template-no-sidebar.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Template Name: No-SideBar
+ */
+?>
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php // Individual Post Styling ?>
+		<?php if( has_post_thumbnail() ) : ?>
+			<div id="imagen-destacada">
+				<?php the_post_thumbnail( 'imagen-destacada' ); ?>
+				<h2><?php the_title(); ?></h2>
+			</div>
+		<?php else: ?>
+			<h2 id="no-imagen-destacada">
+				<?php the_title(); ?>
+			</h2>
+		<?php endif; ?>
+
+		<div id="primary" class="primary no-sidebar post-<?php the_ID(); ?>">
+	        <?php the_content(); ?>
+		</div>
+	<?php endwhile; ?>
+
+	<?php // Navigation ?>
+<?php else : ?>
+	<?php // No Posts Found ?>
+	<div id="primary" class="primary no-sidebar"></div>
+<?php endif; ?>
+
+<?php get_footer(); ?>
