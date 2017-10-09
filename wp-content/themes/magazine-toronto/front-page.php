5c72d4e71bac6e462f65fc4f679a4881f5fe48b8
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-10-09 18:14:36 -0500

    Agrega estilos para la sección de: información y los dos últimos consejos publicados, en la página principal

--- wp-content/themes/magazine-toronto/front-page.php
+++ wp-content/themes/magazine-toronto/front-page.php
@@ -2,18 +2,19 @@
     <div id="que-visitar">
         <?php dynamic_sidebar( 'front-page' ); ?>
     </div>
-    <div class="informacion-consejos">
-        <div class="informacion">
+    <div class="clearfix"></div>
+    <div id="info-and-tips">
+        <div class="information">
             <?php
                 $informacion = new WP_Query( 'page_id=9' ); # Hacemos un llamado al Id de la página de información
                 while( $informacion -> have_posts() ) : $informacion -> the_post();
             ?>
-                <h3><?php the_title(); ?></h3>
+                <h2 class="tips"><?php the_title(); ?></h2>
                 <?php the_excerpt(); ?>
             <?php endwhile; wp_reset_postdata(); ?>
         </div>
-        <div class="consejos">
-            <h2>Consejos para viajar a Canadá</h2>
+        <div class="last-tips">
+            <h2 class="tips">Consejos para viajar a Canadá</h2>
             <?php
                 $args = array(
                     'cat'            => 8,      # ID de la categoría que se desea desplegar
@@ -25,9 +26,13 @@
                 $consejos = new WP_Query( $args );
                 while( $consejos -> have_posts() ) : $consejos -> the_post();
             ?>
-                <?php the_post_thumbnail( 'imagen-guia-toronto' ); ?>
-                <h3><?php the_title(); ?></h3>
-                <?php the_excerpt(); ?>
+                <div class="tip">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_post_thumbnail( 'imagen-guia-toronto' ); ?>
+                        <h3><?php the_title(); ?></h3>
+                    </a>
+                    <?php the_excerpt(); ?>
+                </div>
             <?php endwhile; wp_reset_postdata(); ?>
         </div>
     </div>
